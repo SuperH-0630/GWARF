125769aa7083a39ed8d89a8c3d8a715b6e3c4a47
SongZihuan <jimsong2010@163.com>
2020-04-07 08:26:00 +0800

    add yacc and lex file

---
+++ y.tab.h
@@ -0,0 +1,113 @@
+/* A Bison parser, made by GNU Bison 3.5.3.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    NUMBER = 258,
+    STRING = 259,
+    VAR = 260,
+    ADD = 261,
+    SUB = 262,
+    DIV = 263,
+    MUL = 264,
+    EQ = 265,
+    LESS = 266,
+    MORE = 267,
+    RB = 268,
+    LB = 269,
+    RP = 270,
+    LP = 271,
+    WHILE = 272,
+    STOP = 273,
+    POW = 274
+  };
+#endif
+/* Tokens.  */
+#define NUMBER 258
+#define STRING 259
+#define VAR 260
+#define ADD 261
+#define SUB 262
+#define DIV 263
+#define MUL 264
+#define EQ 265
+#define LESS 266
+#define MORE 267
+#define RB 268
+#define LB 269
+#define RP 270
+#define LP 271
+#define WHILE 272
+#define STOP 273
+#define POW 274
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 5 "cwarf_yacc.y"
+
+    int int_value;
+    double double_value;
+    char *string_value;
+
+#line 101 "y.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
